c6814a5cd70d0b876588c5d9104e5a837d18c398
jojo0severo <joaovbsevero@gmail.com>
2019-09-10 16:53:31 -0300

    Finished neural network implementation

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,5 +1,6 @@
 from scipy.special import expit as sigmoid
 import numpy as np
+np.random.seed(10)
 
 
 class NeuralNetwork:
@@ -13,7 +14,7 @@ class NeuralNetwork:
         self.weights = []
 
         for i in range(len(neurons_by_layer) - 1):
-            self.weights.append(np.random.normal(0.0, neurons_by_layer[i] ** -0.0001,  size=(neurons_by_layer[i], neurons_by_layer[i+1])))
+            self.weights.append(np.random.normal(0.0, neurons_by_layer[i] ** -0.01,  (neurons_by_layer[i], neurons_by_layer[i+1])))
 
         return self.weights
 
@@ -22,13 +23,11 @@ class NeuralNetwork:
 
         if use_batch:
             for i in range(self.max_iterations):
-                for batch_f, batch_t in zip(np.array_split(x, 20), np.array_split(y, 20)):
+                for batch_f, batch_t in zip(np.array_split(x, 30), np.array_split(y, 30)):
                     batch_deltas = None
                     for f, t in zip(batch_f, batch_t):
                         outputs = self.feed_forward(f)
                         deltas = self.back_propagation(outputs, f, t)
-                        for d in deltas:
-                            d = np.clip(d, -200, 200)
                         if batch_deltas is None:
                             batch_deltas = deltas
                         else:
@@ -42,8 +41,6 @@ class NeuralNetwork:
                 for a, b in zip(x, y):
                     outputs = self.feed_forward(a)
                     deltas = self.back_propagation(outputs, a, b)
-                    for d in deltas:
-                        d = np.clip(d, -200, 200)
                     self.update_weights(deltas, length)
 
     def predict(self, x):
@@ -52,9 +49,9 @@ class NeuralNetwork:
             output = self.activation_function(np.dot(a, self.weights[0]))
 
             for layer in self.weights[1:]:
-                output = np.dot(output, layer)
+                output = self.activation_function(np.dot(output, layer))
 
-            if output[0] > 0:
+            if output[0] > 0.5:
                 results.append(1)
             else:
                 results.append(0)
@@ -62,18 +59,22 @@ class NeuralNetwork:
         return results
 
     def feed_forward(self, x):
-        outputs = [self.activation_function(np.dot(x, self.weights[0]))]
+        output = self.activation_function(np.dot(x, self.weights[0]))
+        outputs = [output]
 
         for layer in self.weights[1:]:
-            outputs.append(np.dot(outputs[-1], layer))
+            output = self.activation_function(np.dot(output, layer))
+            outputs.append(output)
 
         return outputs
 
     def back_propagation(self, outputs, x, y):
-        hidden_errors = [outputs[-1] - y]
+        error = outputs[-1] - y
+        hidden_errors = [error]
 
         for layer, output in zip(reversed(self.weights), reversed(outputs[:-1])):
-            hidden_errors.append(np.dot(hidden_errors[-1], layer.T) * output * (1 - output))
+            error = np.dot(error, layer.T) * output * (1 - output)
+            hidden_errors.append(error)
 
         deltas = [hidden_errors[-1] * x[:, None]]
         for error, output in zip(reversed(hidden_errors[:-1]), outputs[1:]):
@@ -83,27 +84,26 @@ class NeuralNetwork:
 
     def update_weights(self, delta, length):
         for layer, delt in zip(self.weights, delta):
-            layer += self.learning_rate * delt / length
+            layer -= self.learning_rate * delt / length
 
 
 def generate_data(target):
     features = []
     for t in target:
         if t == 0:
-            features.append([np.random.randint(10, 20), np.random.randint(10, 20)])
+            features.append([np.random.randint(5, 10), np.random.randint(5, 10)])
         else:
-            features.append([np.random.randint(50, 80), np.random.randint(50, 80)])
+            features.append([np.random.randint(20, 30), np.random.randint(20, 30)])
 
     return features
 
 
 if __name__ == '__main__':
-    for _ in range(15):
-        n = NeuralNetwork([2, 10, 10, 4, 4, 2, 2, 1])
-        target = np.array(np.random.choice([0, 1], size=(1000, 1), p=[3./4, 1./4]))
-        features = np.array(generate_data(target))
+    target = np.array(np.random.choice([0, 1], size=(1000, 1), p=[2. / 3, 1. / 3]))
+    features = np.array(generate_data(target))
 
-        n.train(features, target, False)
-        preds = n.predict(features)
-        acc = (preds == target).mean()
-        print(acc * 100)
+    nn = NeuralNetwork([2, 150, 150, 1], learning_rate=0.1, max_iterations=1000)
+    nn.train(features, target, False)
+    preds = nn.predict(features)
+
+    print((preds == target).mean() * 100)
